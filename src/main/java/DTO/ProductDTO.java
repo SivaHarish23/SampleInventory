c0464c9d29e0e2a6df002c1190b4f84ff7a6307d
SivaHarish23 <siva.harish.murugesan@gmail.com>
2025-05-27 11:54:43 +0530

    feat : Product CRUD

--- src/main/java/DTO/ProductDTO.java
+++ src/main/java/DTO/ProductDTO.java
@@ -1,68 +1,106 @@
 package DTO;
 
-
 import java.math.BigDecimal;
 
 public class ProductDTO {
     private int id;
     private String name;
-    private BigDecimal costPrice;
-    private BigDecimal sellingPrice;
-    private int stockInHand;
-    private int committedStock;
-    private int orderedStock;
-    private int openingStock;
+    private BigDecimal cost_price;
+    private BigDecimal selling_price;
+    private int stock_in_hand;
+    private int committed_stock;
+    private int ordered_stock;
+    private int opening_stock;
 
-    public ProductDTO() {}
-
-    public ProductDTO(int id, String name, BigDecimal costPrice, BigDecimal sellingPrice,
-                      int stockInHand, int committedStock, int orderedStock, int openingStock) {
-        this.id = id;
-        this.name = name;
-        this.costPrice = costPrice;
-        this.sellingPrice = sellingPrice;
-        this.stockInHand = stockInHand;
-        this.committedStock = committedStock;
-        this.orderedStock = orderedStock;
-        this.openingStock = openingStock;
+    public ProductDTO() {
     }
 
-    // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public ProductDTO(int id, String name, BigDecimal cost_price, BigDecimal selling_price, int stock_in_hand, int committed_stock, int ordered_stock, int opening_stock) {
+        this.id = id;
+        this.name = name;
+        this.cost_price = cost_price;
+        this.selling_price = selling_price;
+        this.stock_in_hand = stock_in_hand;
+        this.committed_stock = committed_stock;
+        this.ordered_stock = ordered_stock;
+        this.opening_stock = opening_stock;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public int getId() {
+        return id;
+    }
 
-    public BigDecimal getCostPrice() { return costPrice; }
-    public void setCostPrice(BigDecimal costPrice) { this.costPrice = costPrice; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public BigDecimal getSellingPrice() { return sellingPrice; }
-    public void setSellingPrice(BigDecimal sellingPrice) { this.sellingPrice = sellingPrice; }
+    public String getName() {
+        return name;
+    }
 
-    public int getStockInHand() { return stockInHand; }
-    public void setStockInHand(int stockInHand) { this.stockInHand = stockInHand; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public int getCommittedStock() { return committedStock; }
-    public void setCommittedStock(int committedStock) { this.committedStock = committedStock; }
+    public BigDecimal getCost_price() {
+        return cost_price;
+    }
 
-    public int getOrderedStock() { return orderedStock; }
-    public void setOrderedStock(int orderedStock) { this.orderedStock = orderedStock; }
+    public void setCost_price(BigDecimal cost_price) {
+        this.cost_price = cost_price;
+    }
 
-    public int getOpeningStock() { return openingStock; }
-    public void setOpeningStock(int openingStock) { this.openingStock = openingStock; }
+    public BigDecimal getSelling_price() {
+        return selling_price;
+    }
+
+    public void setSelling_price(BigDecimal selling_price) {
+        this.selling_price = selling_price;
+    }
+
+    public int getStock_in_hand() {
+        return stock_in_hand;
+    }
+
+    public void setStock_in_hand(int stock_in_hand) {
+        this.stock_in_hand = stock_in_hand;
+    }
+
+    public int getCommitted_stock() {
+        return committed_stock;
+    }
+
+    public void setCommitted_stock(int committed_stock) {
+        this.committed_stock = committed_stock;
+    }
+
+    public int getOrdered_stock() {
+        return ordered_stock;
+    }
+
+    public void setOrdered_stock(int ordered_stock) {
+        this.ordered_stock = ordered_stock;
+    }
+
+    public int getOpening_stock() {
+        return opening_stock;
+    }
+
+    public void setOpening_stock(int opening_stock) {
+        this.opening_stock = opening_stock;
+    }
 
     @Override
     public String toString() {
         return "ProductDTO{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", costPrice=" + costPrice +
-                ", sellingPrice=" + sellingPrice +
-                ", stockInHand=" + stockInHand +
-                ", committedStock=" + committedStock +
-                ", orderedStock=" + orderedStock +
-                ", openingStock=" + openingStock +
+                ", cost_price=" + cost_price +
+                ", selling_price=" + selling_price +
+                ", stock_in_hand=" + stock_in_hand +
+                ", committed_stock=" + committed_stock +
+                ", ordered_stock=" + ordered_stock +
+                ", opening_stock=" + opening_stock +
                 '}';
     }
 }
